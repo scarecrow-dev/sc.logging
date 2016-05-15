edf3f9cd4368796450898fe2437699c3deeddad9
Manuel Bothner <manuel.bothner@web.de>
2016-05-15 11:46:09 +0200

    ### Get jacoco running! ########################################################
     - IMPORTANT: Jacoco is only working if you have defined test! So you need 
       minimum one test!
     - Add the class LevelTest which contains a simple test for the Level class
    
    ################################################################################

---
+++ src/test/java/sc/util/logging/LevelTest.java
@@ -0,0 +1,19 @@
+package sc.util.logging;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LevelTest {
+	
+	private Level level;
+	
+	@Before
+	public void setUp() {
+		level = Level.DETAIL;
+	}
+	
+	@Test
+	public void test() {
+		
+	}
+}
